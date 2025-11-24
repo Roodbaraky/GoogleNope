b67be0b9ddd1fefda9a90813366f87cf64b10723
roodbaraky <kooroshr@hotmail.com>
2025-11-24 15:00:57 +0000

    Complete basic API skeleton

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/notes", getNotes)
+	router.POST("/notes", postNotes)
+	router.GET("/notes/:id", getNoteById)
 
 	router.Run("localhost:8080")
 }
@@ -39,4 +41,25 @@ var notes = []note{
 
 func getNotes(context *gin.Context) {
 	context.JSON(http.StatusOK, notes)
+}
+
+func postNotes(context *gin.Context) {
+	var newNotes []note
+	if err := context.BindJSON(&newNotes); err != nil {
+		return
+	}
+
+	notes = append(notes, newNotes...)
+	context.IndentedJSON(http.StatusCreated, newNotes)
+}
+
+func getNoteById(context *gin.Context) {
+	for _, note := range notes {
+		if note.ID.String() == context.Param("id") {
+			context.IndentedJSON(http.StatusOK, note)
+			return
+		}
+	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Note not found"})
+	return
 }
